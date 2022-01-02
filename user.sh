fff4381ecb8bcbdcaeb30b3b4c2669222cb43e68
waso99 <43682410+waso99@users.noreply.github.com>
2022-01-02 04:46:27 -0600

    Add files via upload

---
+++ user.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Appearance pacakges
+
+#Ms-fonts
+sudo apt install ttf-mscorefonts-installer
+
+# Layan Cursors
+mkdir -p $HOME/build
+cd "$HOME/build"
+git clone https://github.com/vinceliuice/Layan-cursors
+cd Layan-cursors
+sudo ./install.sh
