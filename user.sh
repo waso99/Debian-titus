97db6fa2b09d3947ce90ea04e2d96621195d8d18
waso99 <43682410+waso99@users.noreply.github.com>
2022-01-02 16:40:06 +0300

    Update user.sh

--- user.sh
+++ user.sh
@@ -5,6 +5,11 @@
 #Ms-fonts
 sudo apt install ttf-mscorefonts-installer
 
+#wallpaper
+mkdir -p $HOME/Pictures/wallpaper
+cd $HOME/Pictures/wallpaper
+git clone https://github.com/waso99/Debian-titus/tree/main/wallpaper
+
 # Layan Cursors
 #mkdir -p $HOME/build
 #cd "$HOME/build"
