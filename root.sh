9f116f7be62e1e2d2f83de8d49cc3aa61b9573ce
waso99 <43682410+waso99@users.noreply.github.com>
2022-01-04 12:59:20 -0600

    neovim

--- root.sh
+++ root.sh
@@ -26,7 +26,7 @@ apt update
 #apt install unzip picom bspwm polybar sddm rofi kitty thunar neovim flameshot neofetch sxhkd git lxpolkit lxappearance xorg -y
 #apt install papirus-icon-theme lxappearance fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls -y
 
-apt install unzip picom i3 polybar nitrogen rofi kitty terminator thunar neofetch sxhkd git lxpolkit lxappearance xorg -y
+apt install unzip picom i3 polybar nitrogen rofi kitty terminator thunar neovim neofetch sxhkd git lxpolkit lxappearance xorg -y
 apt install ttf-mscorefonts-installer papirus-icon-theme fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls -y
 
 # Download Karitim-Dark-GTK Theme
