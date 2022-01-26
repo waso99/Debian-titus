e09abb119f4d4868bb69bf85d02930ade2dbf3ed
waso99 <43682410+waso99@users.noreply.github.com>
2022-01-26 12:12:51 +0300

    update
    update

---
+++ dotfiles/rofi/powermenu.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+rofi_command="rofi -theme $HOME/.config/rofi/configs/powermenu.rasi"
+
+uptime=$(uptime -p | sed -e 's/up //g')
+cpu=$($HOME/.config/rofi/configs/usedcpu)
+memory=$($HOME/.config/rofi/configs/usedram)
+
+# Options
+shutdown=""
+reboot=""
+lock=""
+suspend=""
+logout=""
+
+# Variable passed to rofi
+options="$shutdown\n$reboot\n$lock\n$suspend\n$logout"
+
+chosen="$(echo -e "$options" | $rofi_command -p "祥  $uptime  |     $cpu  |  ﬙  $memory " -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+			systemctl poweroff
+        ;;
+    $reboot)
+			systemctl reboot
+        ;;
+    $lock)
+		if [[ -f /usr/bin/i3lock ]]; then
+			i3lock
+		elif [[ -f /usr/bin/betterlockscreen ]]; then
+			betterlockscreen -l
+		fi
+        ;;
+    $suspend)
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+        ;;
+    $logout)
+				i3-msg exit
+        ;;
+esac
