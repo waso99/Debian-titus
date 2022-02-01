b83d4bc3395164e6eb9d0d2245dc5b33f2f79315
waso99 <43682410+waso99@users.noreply.github.com>
2022-02-01 07:18:21 +0300

    Update launch.sh

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -6,5 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch the bar
-polybar -q -r primary -c "$DIR"/config.ini &
-polybar -q -r secondary -c "$DIR"/config.ini &
+polybar -q -r primary -c "$DIR"/config &
+polybar -q -r secondary -c "$DIR"/config &
