d5c31b309f27b5344165ee0312a2e2cb63a047df
waso99 <43682410+waso99@users.noreply.github.com>
2022-02-10 02:34:45 -0500

    Update root.sh

--- root.sh
+++ root.sh
@@ -24,7 +24,7 @@ apt update
 
 # Add base packages
 apt install xorg bashtop psmisc firefox gedit unzip picom i3 polybar nitrogen rofi kitty terminator thunar neovim neofetch sxhkd lxpolkit lxappearance -y
-apt install ttf-mscorefonts-installer papirus-icon-theme fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls bmon network-manager network-manager-gnome -y
+apt install conky-all ttf-mscorefonts-installer papirus-icon-theme fonts-noto-color-emoji fonts-firacode fonts-font-awesome libqt5svg5 qml-module-qtquick-controls bmon network-manager network-manager-gnome -y
 
 # Download Karitim-Dark-GTK Theme
 cd /usr/share/themes/
